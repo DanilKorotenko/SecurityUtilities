4dbcf63db7b625e251982052343d2009fe6675b7
Danil Korotenko <danil.korotenko@gmail.com>
2025-10-01 19:41:33 +0300

    Added code sign utility target.

---
+++ codeSignUtility/main.cpp
@@ -0,0 +1,16 @@
+//
+//  main.cpp
+//  codeSignUtility
+//
+//  Created by Danil Korotenko on 10/1/25.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+    std::cout << "Hello, Code sign!\n";
+
+
+    return EXIT_SUCCESS;
+}
